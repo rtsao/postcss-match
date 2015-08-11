b2d58d51539dd3e0a77c75062a2d8debe919bbfa
Ryan Tsao <rtsao@uber.com>
2015-08-10 20:36:49 -0700

    Updated tests, project cleanup

--- index.js
+++ index.js
@@ -6,12 +6,7 @@ function getPatterns(str) {
     return str.replace(/=>|,|\s/g, '').split('|');
 }
 
-module.exports = postcss.plugin('postcss-match', function plugin(opts) {
-    opts = opts || {};
-
-    // TODO: Customizable "any" pattern (e.g. change `_` to `default`)
-    // TODO: Customizable delimiter (e.g. change `=>` to `:`)
-
+module.exports = postcss.plugin('postcss-match', function plugin() {
     return function process(css) {
         css.eachAtRule('match', function processMatch(rule) {
             var value = rule.params;
@@ -24,7 +19,6 @@ module.exports = postcss.plugin('postcss-match', function plugin(opts) {
                     }
                 });
             });
-
             if (!found) {
                 rule.removeSelf();
             }
---
+++ test/index.js
@@ -0,0 +1,48 @@
+var fs      = require('fs');
+var path    = require('path');
+var test    = require('tape');
+var postcss = require('postcss');
+var plugin  = require('../');
+
+var FIXTURES_DIR = './test/fixtures';
+
+function isTestFixture(filename) {
+    return filename.match(/^((?!\.expected).)*\.css$/);
+}
+
+function expectedName(filename) {
+    return filename.replace(/\.css$/, '.expected.css');
+}
+
+function getFixture(filename) {
+    return fs.readFileSync(path.join(FIXTURES_DIR, filename), 'utf8');
+}
+
+function getTestName(filename) {
+    return filename.replace(/(-|\.css$)/g, ' ');
+}
+
+function processCss(source, resultHandler) {
+    postcss([ plugin() ]).process(source).then(function (result) {
+        resultHandler(result);
+        done();
+    }).catch(function (error) {
+        done(error);
+    });
+}
+
+function runTest(filename) {
+    var input = getFixture(filename);
+    var expected = getFixture(expectedName(filename));
+    processCss(input, function handleResult(result) {
+        test(getTestName(filename), function (t) {
+            t.plan(2);
+            t.equal(result.css, expected, 'output matches expected');
+            t.equal(result.warnings().length, 0, 'no warnings');
+            t.end();
+        });
+    });
+}
+
+var tests = fs.readdirSync(FIXTURES_DIR).filter(isTestFixture);
+tests.forEach(runTest);
